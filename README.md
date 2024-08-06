35242b62ee485a3e4294c132f9853ed3e75ceaa4
pandeyakash <pandeyakash0502@gmail.com>
2024-08-06 22:34:21 +0530

    Added comments and readme file

---
+++ README.md
@@ -0,0 +1,5 @@
+# ES6 Class
+
+## Overview
+
+This project defines an `Iphone4` class in JavaScript using ES6 syntax. The class models a simplified version of an iPhone 4, including properties such as ASIN, color, display, camera, and Bluetooth. It also includes methods to simulate basic phone functionalities.
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+// Define a class Iphone4
 class Iphone4 {
+  // Constructor to initialize properties
   constructor(ASIN, color, display, camera, bluetooth) {
     this.ASIN = ASIN;
     this.color = color;
@@ -7,41 +9,44 @@ class Iphone4 {
     this.bluetooth = bluetooth;
   }
 
+  // Method to simulate dialing a phone
   dial() {
     return `tring.. tring...`;
   }
 
+  // Method to simulate sending a message
   sendMessage() {
     return `Sending message...`;
   }
 
+  // Method to simulate clicking the camera
   cameraClick() {
     return "Camera clicked";
   }
 
+  // Method to simulate connecting to Bluetooth
   connectBluetooth() {
     return `Bluetooth connected successfully...`;
   }
 }
 
+// Create an instance of Iphone4
 let i4 = new Iphone4(
-  1,
-  "B09X67JBQV",
-  7.8,
-  "IOS",
-  "128mb",
-  "Gray",
-  "90mm",
-  "2.0 MP",
-  "5.1"
+  1, // ASIN
+  "Gray", // color
+  7.8, // display size
+  "2.0 MP", // camera resolution
+  "5.1" // Bluetooth version
 );
 
+// Log the properties to the console
 console.log(i4.ASIN); // 1
-console.log(i4.color); // B09X67JBQV
+console.log(i4.color); // Gray
 console.log(i4.display); // 7.8
-console.log(i4.camera); // IOS
-console.log(i4.bluetooth); // 128mb
+console.log(i4.camera); // 2.0 MP
+console.log(i4.bluetooth); // 5.1
 
+// Call and log the methods to the console
 console.log(i4.dial()); // "tring.. tring..."
 console.log(i4.sendMessage()); // "Sending message..."
 console.log(i4.cameraClick()); // "Camera clicked"
